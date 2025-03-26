7c9b98447202791738f6503131dc11e947aacf24
Prakharrr47 <gprakhar4703@gamil.com>
2025-03-26 19:19:07 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='GenAI Project prj2',
+    version='0.0.0',
+    author='Prakhar Gupta',
+    author_email= 'prakhar.gupta.23cse@bmu.edu.in',
+    packages= find_packages(),
+    install_requires = []
+    
+)
